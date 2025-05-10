08f537e3be757a86dfbede99303d0e1aa919683c
safsafmedraed <safsafraed@gmail.com>
2025-05-10 17:04:07 +0100

    trying to fix styles in production

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/pro-portfolio',
-  assetPrefix: '/pro-portfolio/',
+  output: 'export', // Required for static export
+  // build for static export
+  basePath: '/pro-portfolio', // Match repo name
+  // assetPrefix: '/pro-portfolio/', // Fixes CSS/JS paths
+  // trailingSlash: true, // Ensures `/` endings
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  // images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
